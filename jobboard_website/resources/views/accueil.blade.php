ba22331693013f128f3c70d42ce6d8d35a1a96f0
Bastien Laffon <>
2019-02-12 17:03:08 +0100

    Mise en page de l'Accueil

--- jobboard_website/resources/views/accueil.blade.php
+++ jobboard_website/resources/views/accueil.blade.php
@@ -12,16 +12,18 @@
 
             </figure>
         </div>
-        <p> Ce JobBoard a pour but de mettre en relation des élèves à la recherche d'un stage et des entreprises cherchant des stagiaire.<br><br>
+        <br>
+        <p> Ce JobBoard a pour but de mettre en relation des élèves à la recherche d'un stage et des entreprises cherchant des stagiaires.<br><br>
             Si vous êtes un élève , nous vous invitons à créer un compte et à remplir le formulaire d'incription pour que les entreprises puissent connaitre vos compétences. <br> <br>
-            Si vous êtes une entreprise, nous vous invitons a créer des offres aux quelles nos étudiants pourront repondre mais également a vous inscrire à nos job dating afin de venir rencontrer nos étudiants. <br>
+            Si vous êtes une entreprise, nous vous invitons a créer des offres aux quelles nos étudiants pourront repondre mais également à vous inscrire à nos Job Dating afin de venir rencontrer nos étudiants. <br>
         </p>
-
-        <h2>L'IUT informatique de Lens forme des étudiants dans le domaine de l'informatique !</h2>
+        <div class="jumbotron" >
+             <div class="border border-success">
+            <h2>L'IUT informatique de Lens forme des étudiants dans ce domaine </h2><br>
 
         <h3> Le DUT : une formation complète !</h3>
 
-        <p> Les étudiants suivent une formation, sur deux années après le baccalauréat, qui les formes dans plusieurs domaine : </p><br>
+        <p> Les étudiants suivent une formation, sur deux années après le baccalauréat, qui les formes dans plusieurs domaines : </p><br>
 
         <table class="table">
             <thead>
@@ -34,7 +36,7 @@
             <tbody>
             <tr>
                 <td>MySQL</td>
-                <td>HTML /CSS</td>
+                <td>HTML / CSS</td>
                 <td>Java</td>
                 <td>Adressage IP</td>
             </tr>
@@ -45,30 +47,31 @@
                 <td> Routage de Machine</td>
             </tr>
             <tr>
-            <td> </td>
+                <td> sqlite </td>
                 <td> Javascript / Angular</td>
                 <td> C++</td>
-                <td> </td>
+                <td>  </td>
             </tr>
             </tbody>
             </thead>
         </table>
 
-        <p>    Ils savent également utilisé <b>GitHub</b> , <b>Trello</b> et des IDE comme <b>Eclipse</b>, <b>IntelIj IDEA</b> ou encore <b>PhpStorm</b> ! <br> <br>
+        <p>    Ils savent également utiliser <b>GitHub</b> , <b>Trello</b> et des IDE comme <b>Eclipse</b>, <b>IntelIj IDEA</b> ou encore <b>PhpStorm</b> ! <br> <br>
 
             Mais chaque profil est unique!  Si vous êtes une entreprise qui cherche dans l'un de ces domaines, n'hésitez pas à créer une offre pour qu'ils puissent vous contacter !</p>
-
-        <h3> Le Stage </h3>
-        <p>
-            Afin de valider leur DUT nos étudiants recherche un stage dans le <b>développement</b> informatique. <br>
+            <h3> Le Stage </h3>
+         <p>
+            Afin de valider leurs DUT nos étudiants recherchent un stage dans le <b>développement</b> informatique. <br>
             Il a pour but de mettre en pratique ce qu'ils ont appris durant les deux années de cours. <br>
-            Mais nos étudiants sont également friant d'apprendre de nouvelles choses et apprendrons volontié de nouveaux langages durant leur stages ! <br>
-            Leur stage doit avoir une durée minimum de dix semaines à partir du <b>8 avril 2019.</b>
-        </p>
+            Mais nos étudiants sont également friant d'apprendre de nouvelles choses et apprendrons volontié de nouveaux langages durant leurs stages ! <br>
+            Leurs stage doit avoir une durée minimum de dix semaines à partir du <b>8 avril 2019.</b>
+         </p>
 
-        <h3>     Si vous voulez rencontrer nos étudiant c'est possible !</h3>
+         <h3> Si vous voulez rencontrer nos étudiants c'est possible !</h3>
 
-        <p> Chaque année nous organisons des <b>Job-Dating</b> afin de faciliter le dialogue entre les étudiants et les entreprise.<br>
-            Si votre entreprise est  intéressée venez vous inscrire à l'un de nos Job-Dating , <b>nos étudiants  vous attendes !</b></p>
-        </div>
+         <p> Chaque année nous organisons des <b>Job Dating</b> afin de faciliter le dialogue entre les étudiants et les entreprises.<br>
+            Si votre entreprise est intéressée venez vous inscrire à l'un de nos Job Dating , <b>nos étudiants vous attendes !</b></p>
+         </div>
+    </div>
+        
 @endsection
