e17209574fd30eff5be6e22f52846d89e5ea0887
Bastien Laffon <>
2019-02-13 17:00:19 +0100

    Mise en place de la page Error 404

---
+++ jobboard_website/resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> 404 Page Not Found</title>
+</head>
+<body>
+<link href="{{ asset('css/404.css') }}" rel="stylesheet">
+<h1>Erreur 404:</h1>
+<p>Oopss cette adresse n'existe pas</p>
+</body>
+</html>
