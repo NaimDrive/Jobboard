d7805420fc4a9f084fa45cece6356aa1c664faca
Bastien Laffon <>
2019-02-14 14:52:47 +0100

    Correction bug de la navbar

---
+++ jobboard_website/resources/views/layouts/masterAccueil.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Jobboard IUT de Lens</title>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{  asset ('css/bootstrap.css')  }}"/>
+</head>
+<body>
+@include('layouts.headerAccueil')
+@yield('content')
+@yield('javaScript')
+@include('layouts.footer')
+</body>
+</html>
