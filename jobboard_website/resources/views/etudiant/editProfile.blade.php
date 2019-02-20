0286134c682ea390ff076e51678918422986fed4
JeanAbdel <loanenems@gmail.com>
2019-02-20 16:02:47 +0100

    editProfile pas fonctionnel

--- jobboard_website/resources/views/etudiant/editProfile.blade.php
+++ jobboard_website/resources/views/etudiant/editProfile.blade.php
@@ -9,10 +9,9 @@
 
                 <!-- DEBUT DU FORMULAIRE DE PHOTO DE PROFILE -->
 
-                <img src="{{asset($image)}}" alt="photo de profile" width="500" height="500"/>
+                <img src="{{asset($user->picture)}}" alt="photo de profile" width="500" height="500"/>
 
-
-                <form enctype="multipart/form-data" method="POST" action="{{route('enregistrer_image')}}">
+                <form enctype="multipart/form-data" method="POST" action="{{route('enregistrer_modifs')}}">
                     <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
                     {!! csrf_field() !!} <!-- toujours ajouter dans un formulaire, sinon error 419 -->
                     <fieldset>
@@ -23,27 +22,22 @@
                             <button type="submit" class="btn btn-success col-lg-2">Ajouter</button>
                         </div>
                     </fieldset>
-                </form>
 
                 <!-- DEBUT DU FORMULAIRE D' IDENTITE -->
 
 
 
-                <form method="POST" action="{{route('enregistrer_identite')}}">
-                    <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
-                    {!! csrf_field() !!} <!-- toujours ajouter dans un formulaire, sinon error 419 -->
                     <fieldset>
                         <legend>Identité</legend>
                         <div class="form-group">
                             <div class="row">
-                                <div class="col-lg-6"><input type="text" class="form-control" id="nom" name="nom" value="{{old("nom")}}" aria-describedby="infoComp" placeholder="Nom"></div>
-                                <div class="col-lg-6"><input type="text" class="form-control" id="prenom" name="prenom" value="{{old("prenom")}}" aria-describedby="infoComp" placeholder="Prenom"></div>
+                                <div class="col-lg-6"><input type="text" class="form-control" id="nom" name="nom" value="{{$user->nom}}" aria-describedby="infoComp" placeholder="Nom"></div>
+                                <div class="col-lg-6"><input type="text" class="form-control" id="prenom" name="prenom" value="{{$user->prenom}}" aria-describedby="infoComp" placeholder="Prenom"></div>
                             </div>
                             <br>
                             <button type="submit" class="btn btn-success col-lg-2">Ajouter</button>
                         </div>
                     </fieldset>
-                </form>
 
                 <!-- DEBUT DU FORMULAIRE DE COMPETENCES -->
 
@@ -61,13 +55,10 @@
                     @foreach($competence as $comp) <!-- On génère pour chaque compétence une ligne avec le nom, le niveau et un bouton de suppression -->
                         <tbody>
                         <tr>
-                            <form method="POST" action="{{route('supprimer_competence')}}">
-                                <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
                                 {!! csrf_field() !!} <!-- toujours ajouter dans un formulaire, sinon error 419 -->
                             <th scope="row">{{$comp->nomCompetence}}</th>
                             <td>{{$comp->niveauEstime}}</td>
                             <td><button type="submit" class="btn btn-danger col-lg-8" id="competence_del" name="competence_del" value="{{$comp->nomCompetence}}">X</button></td>
-                            </form>
                         </tr>
                         </tbody>
                         @endforeach
@@ -76,12 +67,9 @@
                 @endif
 
 
-                <form method="POST" action="{{route('enregistrer_competence')}}">
-                    <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
-                    {!! csrf_field() !!}
                     <fieldset>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="competence" name="competence" value="{{old("competence")}}" aria-describedby="infoComp" placeholder="Exemple: Javascript">
+                            <input type="text" class="form-control" id="competence" name="competence" value="" aria-describedby="infoComp" placeholder="Exemple: Javascript">
                             <br>
                             <label for="level">Niveau estimé</label>
                             <select class="form-control" id="level" name="level" value="{{old("level")}}">
@@ -92,7 +80,7 @@
                             </select>
                             <br>
                             <label for="categorie">Catégorie</label>
-                            <select class="form-control" id="categorie" name="categorie" value="{{old("categorie")}}" >
+                            <select class="form-control" id="categorie" name="categorie" value={{old("categorie")}} >
                                 @foreach($categorie as $c) <!-- On génère pour chaque catégorie une option. Si on insère une catégorie dans la table, elle apparait ici -->
                                     <option>{{$c}}</option>
                                 @endforeach
@@ -101,7 +89,6 @@
                             <button type="submit" class="btn btn-success col-lg-2">Ajouter</button>
                         </div>
                     </fieldset>
-                </form>
 
                 <!-- DEBUT DU FORMULAIRE D'EXPERIENCES -->
 
@@ -122,15 +109,11 @@
                 @foreach($experience as $exp) <!-- On génère pour chaque experience une ligne avec le nom, le niveau et un bouton de suppression -->
                     <tbody>
                     <tr>
-                        <form method="POST" action="{{route('supprimer_experience')}}">
-                            <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
-                        {!! csrf_field() !!} <!-- toujours ajouter dans un formulaire, sinon error 419 -->
                             <th scope="row">{{$exp->nom}}</th>
                             <td>{{$exp->dateDebut}}</td>
                             <td>{{$exp->dateFin}}</td>
                             <td>{{$exp->etablissement}}</td>
                             <td><button type="submit" class="btn btn-danger col-lg-8" id="experience_del" name="experience_del" value="{{$exp->nom}}">X</button></td>
-                        </form>
                     </tr>
                     </tbody>
                     @endforeach
@@ -139,27 +122,24 @@
                 @endif
 
 
-                <form method="POST" action="{{route('enregistrer_experience')}}">
-                    <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
-                    {!! csrf_field() !!} <!-- toujours ajouter dans un formulaire, sinon error 419 -->
                     <fieldset>
                         <div class="form-group">
-                            <input type="text" class="form-control" id="intitulePoste" name="intitulePoste" value="{{old("intitulePoste")}}" placeholder="Intitulé du poste (exemple: développeur Web)">
+                            <input type="text" class="form-control" id="intitulePoste" name="intitulePoste" value="" placeholder="Intitulé du poste (exemple: développeur Web)">
                             <br>
-                            <input type="text" class="form-control" id="etablissement" name="etablissement" value="{{old("etablissement")}}" placeholder="Nom de l'entreprise">
+                            <input type="text" class="form-control" id="etablissement" name="etablissement" value="" placeholder="Nom de l'entreprise">
                             <br>
                             <div class="row">
                                 <!-- A MODIFIER CAR INPUT TYPE DATE NON PRIS EN CHARGE PAR SAFARI -->
-                                <div class="col-lg-6">Date de début<input type="date" class="form-control" id="dateDebut" name="dateDebut" value="{{old("dateDebut")}}"></div>
-                                <div class="col-lg-6">Date de fin<input type="date" class="form-control" id="dateFin" name="dateFin" value="{{old("dateFin")}}"></div>
+                                <div class="col-lg-6">Date de début<input type="date" class="form-control" id="dateDebut" name="dateDebut" value="0001-01-01"></div>
+                                <div class="col-lg-6">Date de fin<input type="date" class="form-control" id="dateFin" name="dateFin" value="0001-01-01"></div>
                             </div>
                             <br>
                             <label for="description">Description du poste</label>
-                            <textarea class="form-control" id="description" name="description" value="{{old("description")}}" rows="5" style="resize: none;"></textarea><br>
+                            <textarea class="form-control" id="description" name="description" rows="5" style="resize: none;"></textarea><br>
                             <button type="submit" class="btn btn-success col-lg-2">Ajouter</button>
                         </div>
                     </fieldset>
-                </form>
+
 
                 <!-- DEBUT DU FORMULAIRE DE CENTRES D INTERET -->
 
@@ -175,12 +155,8 @@
                 @foreach($activite as $ac) <!-- On génère pour chaque activite une ligne avec le nom et un bouton de suppression -->
                     <tbody>
                     <tr>
-                        <form method="POST" action="{{route('supprimer_activite')}}">
-                            <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
-                        {!! csrf_field() !!} <!-- toujours ajouter dans un formulaire, sinon error 419 -->
                             <th scope="row">{{$ac}}</th>
                             <td><button type="submit" class="btn btn-danger col-lg-8" id="activite_del" name="activite_del" value="{{$ac}}">X</button></td>
-                        </form>
                     </tr>
                     </tbody>
                     @endforeach
@@ -189,9 +165,6 @@
                 @endif
 
 
-                <form method="POST" action="{{route('enregistrer_activite')}}">
-                    <input id="idEtu" name="idEtu" type="hidden" value={{$id}}>
-                    {!! csrf_field() !!} <!-- toujours ajouter dans un formulaire, sinon error 419 -->
                     <fieldset>
                         <legend>Centres d'intérêt</legend>
                         <div class="form-group">
@@ -199,13 +172,12 @@
                             <button type="submit" class="btn btn-success col-lg-2">Ajouter</button>
                         </div>
                     </fieldset>
-                </form>
+
 
 
                 <!-- DEBUT DU FORMULAIRE DES LIENS -->
 
-                <form method="POST">
-                    {!! csrf_field() !!}
+
                     <div id="liens">
                         <input type="hidden" name="nbLiens" id="compteur">
                         <fieldset>
