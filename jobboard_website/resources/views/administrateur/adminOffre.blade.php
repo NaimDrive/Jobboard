b5758d748af850fd6bb5f86ccc35edc574763e7e
DuhAlex <duhemalexandre1@gmail.com>
2019-02-26 19:09:55 +0100

    admin petite modif

--- jobboard_website/resources/views/administrateur/adminOffre.blade.php
+++ jobboard_website/resources/views/administrateur/adminOffre.blade.php
@@ -7,8 +7,7 @@
     <div class="container text-center">
         <div class="row">
             <div class="col-12">
-                <h1>Offre</h1>
-                <a href="#"> <button class="btn-success" id="btnEntrepriseAdmin">Ajouter une offre</button></a>
+                <h1>Offres</h1>
             </div>
         </div>
 
@@ -17,9 +16,9 @@
                 <div class="card text-center col-12 col-md-3">
                     <div class="card-body">
                         <strong><p class="card-title text-center"> {{$offre->nomOffre}}  {{$offre->natureOffre}}</p></strong>
-                        <a href="#"><button class="btn-primary mb-2">Visionner</button></a><br>
+                        <a href="{{route('afficherOffres')}}"><button class="btn-primary mb-2">Visionner</button></a><br>
                         <a href="#"> <button class="btn-secondary mb-2">Modifier</button></a><br>
-                        <a href="{{route('supprimerUneOffre',[$offre->id])}}"><button class="btn-danger mb-2">Supprimer</button></a><br>
+                        <a href="#"><button class="btn-danger mb-2">Supprimer</button></a><br>
                     </div>
                 </div>
             @endforeach
