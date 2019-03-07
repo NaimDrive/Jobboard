6142e646865005f2b9e066aa6f40b853e036c83f
Elric59 <47299738+Elric59@users.noreply.github.com>
2019-03-07 17:14:36 +0100

    mesOffresUpdate

--- jobboard_website/resources/views/etudiant/mesRecherches.blade.php
+++ jobboard_website/resources/views/etudiant/mesRecherches.blade.php
@@ -5,24 +5,19 @@
     <div class="card mt-4">
         <h1 class="card-header">Mes offres</h1>
         <div class="card-body">
-    <?php
-    $idEtudiant = DB::table('etudiant')->where('idUser',Auth::id())->value('id');
-    $idRecherche = DB::table('offre_cherchee')->where('idEtudiant',$idEtudiant)->pluck('idOffre');
-
-    ?>
     @foreach($idRecherche as $idR)
         <div class="border p-3 mt-2">
         <?php
-        $nomOffre = DB::table('offre')->where('id',$idRecherche)->value('nomOffre');
-        $idEntreprise = DB::table('offre')->where('id',$idRecherche)->value('idEntreprise');
+        $nomOffre = DB::table('offre')->where('id',$idR->idOffre)->value('nomOffre');
+        $idEntreprise = DB::table('offre')->where('id',$idR->idOffre)->value('idEntreprise');
         $nomEntreprise = DB::table('entreprise')->where('id',$idEntreprise)->value('nom');
         ?>
         <p> Nom de l'offre : {{$nomOffre}}</p>
         <p>Nom de l'entreprise: {{$nomEntreprise}}</p>
             <div class="row ml-2">
-                <a href="{{route("afficherUneOffre",["id"=>$idR])}}" class="btn btn-success">Voir l'offre</a>
+                <a href="{{route("afficherUneOffre",["id"=>$idR->id])}}" class="btn btn-success">Voir l'offre</a>
             <div class="ml-5">
-                <a href="{{route('dropOffre',["id"=>$idR])}}" class="btn btn-success">Ne plus sauvegarder l'offre</a>
+                <a href="{{route('dropOffre',["id"=>$idR->id])}}" class="btn btn-success">Ne plus sauvegarder l'offre</a>
             </div>
         </div>
         </div>
