a85a733b8ad0c6f324173089e3f65ae1e32ea952
Elric59 <47299738+Elric59@users.noreply.github.com>
2019-03-16 16:28:30 +0100

    Calcul de la durée entre 2 dates
    Co-Authored-By: bastienlaffon <bastienlaffon@users.noreply.github.com>

--- jobboard_website/resources/views/etudiant/listeRecherches.blade.php
+++ jobboard_website/resources/views/etudiant/listeRecherches.blade.php
@@ -7,21 +7,26 @@
         <div class="card mt-4">
             <h1 class="card-header">Les Recherches de nos étudiants </h1>
             <div class="card-body">
-                @foreach($recherches as $r)
-
+            @foreach($recherches as $r)
+                <?php 
+                $date1 = strtotime($r->dateDebut);
+                $date2 = strtotime ($r->dateFin);
+                $res = round(round(abs( $date2 - $date1  )/60/60/24)/7);
+                ?>
+                <div class="border p-3 mt-2">
+                    <p> {{ $r->etudiant->civilite }} <b>{{$r->etudiant->user->nom}} {{$r->etudiant->user->prenom}}</b></p>
                         <div class="border p-3 mt-2">
-                            <p> {{ $r->etudiant->civilite }} <b>{{$r->etudiant->user->nom}} {{$r->etudiant->user->prenom}}</b></p>
-                                <div class="border p-3 mt-2">
-                                      <p>Souhait: {{$r->souhait}}</p>
-                                      <p>Date de début de stage: {{$r->dateDebut}} </p>
-                                      <p>Date minimum de fin de stage: {{$r->dateFin}} </p>
-                                      <p>Mobilité: {{$r->mobilite}}</p>
-                                        <div class="row ml-2">
-                                             <a class="btn btn-success" href="{{ route('consult_profile',["id"=>$r->etudiant->id]) }} "> Voir le profile</a>
-                                         </div>
+                            <p>Souhait: {{$r->souhait}}</p>
+                            <p>Date de début de stage: {{$r->dateDebut}} </p>
+                            <p>Date minimum de fin de stage: {{$r->dateFin}} </p>
+                            <p>Durée: {{$res}} semaines </p>
+                            <p>Mobilité: {{$r->mobilite}}</p>
+                                <div class="row ml-2">
+                                    <a class="btn btn-success" href="{{ route('consult_profile',["id"=>$r->etudiant->id]) }} "> Voir le profile</a>
                                 </div>
-                             </div>
-                        @endforeach
+                        </div>
+                </div>
+            @endforeach
             </div>
             <div class="mt-3">{{ $recherches->links() }}</div>
 
