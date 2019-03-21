ec758f892c285d8e80b083ee767c9a191f49b987
DuhAlex <duhemalexandre1@gmail.com>
2019-03-21 14:30:14 +0100

    formulaire erreur français annonce et forum

--- jobboard_website/resources/views/forum/modifierUnForum.blade.php
+++ jobboard_website/resources/views/forum/modifierUnForum.blade.php
@@ -5,6 +5,15 @@
     <div class="container">
         <div class="row justify-content-md-center">
             <div class=" col-lg-6">
+                @if ($errors->any())
+                    <div class="alert alert-danger"  style="margin-top: 2rem">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                @endif
                 <form method="POST" action="{{route('enregistrerModifForum',["id"=>$forum->id])}}">
                     {!! csrf_field() !!}
                     <h1>Modifier le forum</h1>
